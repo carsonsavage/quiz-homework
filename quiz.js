4c9fc7e2765779128193f628ecd060b39914c34a
carsonsavage <carsonesavage@gmail.com>
2020-08-29 21:48:45 -0500

    pushing quiz files to git hub

---
+++ quiz.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        title: "What famous band was Harry Styles in to begin his career?",
+        choices: ["New Directions", "One Direction", "Big Time Rush", "5 Seconds of Summer"],
+        answer: "One Direction"
+    },
+
+    {
+        title: "What is his REAL first name?",
+        choices: ["Edward", "Harold", "Harry", "Harrey"],
+        answer: "Harry"
+    },
+
+    {
+        title: "What was the name of his debut solo album?",
+        choices: ["Fine Line", "Harry Styles", "Sweet Creature", "Adore You"],
+        answer: "Harry Styles"
+    },
+
+    {
+        title: "What is Harry's hometown?",
+        choices: ["Holmes Chapel", "Doncaster", "Mullingar", "Bradford"],
+        answer: "Holmes Chapel"
+    },
+
+    {
+        title: "Where did Harry work before joining One Direction?",
+        choices: ["Grocery store", "Clothing Shop", "Nowhere", "Bakery"],
+        answer: "Bakery"
+    },
+
+    {
+        title: "What was Harry's first tattoo of?",
+        choices: ["Butterfly", "Two Birds", "Star", "17BLACK"],
+        answer: "Star"
+    },
+
+
+];
